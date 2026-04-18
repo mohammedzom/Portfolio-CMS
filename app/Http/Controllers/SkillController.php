4291a62c2c4142e73d32b7827d22d46a2ceaf534
mohammedzom <mohammedzomlot2@gmail.com>
2026-04-18 11:32:08 +0300

    feat: sort skills by proficiency and add hot directory to public gitignore

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -25,7 +25,7 @@ class SkillController extends Controller
             $query->where('name', 'like', '%'.request()->search.'%');
         }
 
-        $skills = $query->latest()->paginate(10);
+        $skills = $query->orderBy('proficiency', 'desc')->paginate(10);
 
         return view('admin.skills.index', compact('skills'));
     }
