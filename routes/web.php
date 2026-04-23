95569d7ce5a01197e7e31fd35ac8d038adbc13f7
mohammedzom <mohammedzomlot2@gmail.com>
2026-04-23 23:14:29 +0300

    refactor: replace PortfolioController index route with a direct view return for app.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PortfolioController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('app');
+});
