bd60e1a4dc82f5b1e91f5da869ab5847f4f7a9a0
Mohammed Zomlot <mohammedzomlot2@gmail.com>
2026-04-30 11:56:56 +0300

    chore: remove orphaned files from node_modules directory

--- app/Actions/Achievement/StoreAchievementAction.php
+++ app/Actions/Achievement/StoreAchievementAction.php
@@ -5,15 +5,21 @@ namespace App\Actions\Achievement;
 use App\Models\Achievement;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class StoreAchievementAction
 {
     use AsAction;
 
-    public function handle(array $data, $file = null): Achievement
+    public function handle(array $data, mixed $file = null): Achievement
     {
         return DB::transaction(function () use ($data, $file) {
+            if (isset($data['certificate_url'])) {
+                if ($data['certificate_url'] !== null) {
+                    Storage::disk('public')->delete($data['certificate_url']);
+                }
+            }
             if ($file) {
                 $fileName = $file->getClientOriginalName();
                 $file = $file->storeAs('achievements', $fileName, 'public');
