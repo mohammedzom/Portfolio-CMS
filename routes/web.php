729ff4f6b91a5074b9f4a1334c11e4d64de701cf
Mohammed Zomlot <mohammedzomlot2@gmail.com>
2026-05-03 11:21:05 +0300

    refactor: backup CI/CD workflow and remove catch-all redirect route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('https://mohammedzomlot.dev');
 });
-
-Route::any('{any}', function () {
-    return redirect('https://mohammedzomlot.dev');
-})->where('any', '.*');
